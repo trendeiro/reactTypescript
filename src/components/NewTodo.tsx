0e48cd1ad4369c7d758268fe278a6c6c43310477
trendeiro <torendeiro@gmail.com>
2022-07-07 10:14:43 +0100

    first commit

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,30 @@
+import React, { useContext, useRef } from "react";
+import { TodosContext } from "../store/todos-context";
+import classes from './NewTodo.module.css';
+const NewTodo: React.FC= () => {
+
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+  const todosCtx = useContext(TodosContext);
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    todosCtx.addTodo(enteredText);
+
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
